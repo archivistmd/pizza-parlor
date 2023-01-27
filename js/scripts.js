f01b2704c84d84897d6e130f48d1bbf7b2aa4d00
Mesha <madsketchz@gmail.com>
2023-01-27 14:17:45 -0800

    Add update to handleform, will pull user's selections and make calculations

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,22 @@ function Pie() {
   this.cheese = undefined;
   this.toppingAdd = [];
   this.toppingCost = 0;
-  this.costTotal = 0;
 }
 
 Pie.prototype.addSize = function(sizeSelect) {
   this.size = sizeSelect;
   if (this.size === "personal") {
-    this.sizeCost = "5";
+    this.sizeCost = 5;
   } else if (this.size === "small") {
-    this.sizeCost = "10";
+    this.sizeCost = 10;
   } else if (this.size === "medium") {
-    this.sizeCost = "15";
+    this.sizeCost = 15;
   } else if (this.size === "large") {
-    this.sizeCost = "20";
+    this.sizeCost = 20;
   } else if (this.size === "xlarge") {
-    this.sizeCost = "25";
+    this.sizeCost = 25;
   } else if (this.size === "xxlarge") {
-    this.sizeCost = "35";
+    this.sizeCost = 35;
   }
 }
 
@@ -39,7 +38,7 @@ Pie.prototype.addToppings = function(toppings) {
   this.toppingCost += 2;
 }
 
-Pie.prototype.calculateCostTotal = function() {
+Pie.prototype.calculateTotalCost = function() {
   let total = this.sizeCost + this.toppingCost
   return total;
 }
@@ -47,3 +46,24 @@ Pie.prototype.calculateCostTotal = function() {
 
 
 //user logic
+function handleFormSubmission(event) {
+  event.preventDefault();
+  let pie = new Pie();
+  const size = document.getElementById("size").value;
+  const sauce = document.getElementById("sauce").value;
+  const cheese = document.getElementById("cheese").value;
+  const toppingAdd = document.querySelectorAll("input[name=toppingAdd]checked");
+  const toppingAddArray = Array.from(toppingAdd);
+
+  toppingAddArray.forEach(function(toppings) {
+    pie.addToppingAdd(toppings);
+  });
+  pie.addSize(size);
+  pie.addSauce(sauce);
+  pie.addCheese(cheese);
+  let total = pie.calculateTotalCost();
+}
+
+window.addEventListener("load", function() {
+  documument.querySelector("form#pizza-order").addEventListener("submit", handleFormSubmission);
+});
